15dc97f37dd405ea55110dabf3fe67a8b36d2996
lizett <lizett503@gmail.com>
2022-01-29 21:54:20 -0800

    Initial Commit

---
+++ I-heart-coffee/I_heart_coffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_heart_coffeeApp.swift
+//  I-heart-coffee
+//
+//  Created by Susan Tovar on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct I_heart_coffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
