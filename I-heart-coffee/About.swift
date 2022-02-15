1168e5393ff26948cc80709ca1c4b488de6bc139
lizett <lizett503@gmail.com>
2022-02-15 15:15:19 -0800

    Typos and UI fixed in about view.

--- I-heart-coffee/About.swift
+++ I-heart-coffee/About.swift
@@ -18,14 +18,16 @@ struct About: View {
             Spacer()
             Text("Cups are measured as 6 oz. This is the standard meausurement used by most, if not all, your brewing devices.")
                 .padding()
-            Text("This app uses a the calculation of 6.4 grams per tablespoon. I have measured this myself personally and this is the estimation I use for all my calculations in this app. Weight is measured AFTER coffee beans have been grinded.")
-                .padding()
             Text("Grinder models used currently are as follows:")
                 .padding(5)
             Text("• Baratza Encore")
             Text("• Baratza Virtuoso+")
             Text("• OXO Brew Conical Burr Grinder")
             Text("• Cuisinart Professional Burr Mill")
+                
+            Text("This app uses a customized calculation of 6.4 grams per tablespoon. I have measured this myself personally and this is the estimation I use for all my calculations in this app. Weight is measured AFTER coffee beans have been grinded.")
+                .padding()
+            
                
             Spacer()
         } .font(.system(size: 20))
